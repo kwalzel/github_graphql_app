493de9ba247346b595ca5356bac899b89f87736e
Kyle Walzel <kyle.walzel@broadcom.com>
2022-03-16 12:03:28 -0500

    Initial version - react application that uses GraphQL interface to github.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+const TITLE = 'React GraphQL GitHub Client';
+class App extends Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+    organization: null,
+    errors: null,
+  };
+
+  componentDidMount() {
+    // fetch data
+    this.onFetchFromGitHub();
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+    //the path in the state is set according to user input
+  };
+
+  onSubmit = event => {
+    // fetch data
+    event.preventDefault();
+  };
+
+  onFetchFromGitHub = () => {
+    axiosGitHubGraphQL
+      .post('', { query: GET_ORGANIZATION })
+      .then(result => {
+        console.log(result)
+        this.setState(() => ({
+          organization: result.data.data.organization,
+          errors: result.data.errors,
+        }));
+      });
+  };
+
+  render() {
+    const { path, organization, errors } = this.state;
+    return (
+      <div>
+        <h1>{TITLE}</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input //input field with onChange handler
+            id="url"
+            type="text"
+            value={path}
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <hr />
+        {organization ? (
+          <Organization organization={organization} errors={errors} />
+        ) : (
+          <p>No information yet ...</p>
+        )}
+      </div>
+    );
+  }
+}
+export default App;
+
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <strong>Issues from Organization:</strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
     </div>
   );
-}
-
-export default App;
+};
